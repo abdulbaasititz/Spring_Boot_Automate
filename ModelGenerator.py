bb64f37f88f328cc2b5781f72c0ed02018a89c1d
abdul <itzabdulbaasit@gmail.com>
2021-09-25 12:21:02 +0530

    improvised

--- ModelGenerator.py
+++ ModelGenerator.py
@@ -1,12 +1,12 @@
 print("convert table into spring boot model")
 # input
 #--------------------------------------
-projectName = "uclo.shopee"
-tableName = "SHOPEE_CONFIG"
-className = "ShopeeConfig"
-# if plant name in db means mention 1 eg:(plant_table name) else 0
-plantName = 0
-# if model name and alias name is same except first letter put 0 else 1
+projectName = "uclo.pos"
+tableName = "FINCUSTOMERCREDITNOTEHDR"
+className = "FinCustomerCreditNoteHdr"
+# if plant name in db, mention 1 eg:(plant_table name) else 0
+plantName = 1
+# if model name and alias name are same except first letter put 0 else 1
 aliasName = 1
 #--------------------------------------
 seperator = " "
@@ -36,24 +36,24 @@ for line in lines:
 
     if getVal[1] == "int" or getVal[1] == "smallint" or getVal[1] == "tinyint":
         if aliasName == 0 :
-            writeData.write("\tprivate int "+''.join([getVal[0][0].lower()+getVal[0][1:]])+";\n")
+            writeData.write("\tprivate Integer "+''.join([getVal[0][0].lower()+getVal[0][1:]])+";\n")
         else :
-            writeData.write("\tprivate int "+getVal[3]+";\n")
+            writeData.write("\tprivate Integer "+getVal[3]+";\n")
     elif getVal[1] == "float":
         if aliasName == 0:
-            writeData.write("\tprivate float "+''.join([getVal[0][0].lower()+getVal[0][1:]])+";\n")
+            writeData.write("\tprivate Float "+''.join([getVal[0][0].lower()+getVal[0][1:]])+";\n")
         else:
-            writeData.write("\tprivate float " + getVal[3] + ";\n")
+            writeData.write("\tprivate Float " + getVal[3] + ";\n")
     elif getVal[1] == "double" or getVal[1] == "amount":
         if aliasName == 0:
-            writeData.write("\tprivate double "+''.join([getVal[0][0].lower()+getVal[0][1:]])+";\n")
+            writeData.write("\tprivate Double "+''.join([getVal[0][0].lower()+getVal[0][1:]])+";\n")
         else :
-            writeData.write("\tprivate double " + getVal[3] + ";\n")
+            writeData.write("\tprivate Double " + getVal[3] + ";\n")
     elif getVal[1].find("decimal") != -1:
         if aliasName == 0 :
-            writeData.write("\tprivate float "+''.join([getVal[0][0].lower()+getVal[0][1:]])+";\n")
+            writeData.write("\tprivate Float "+''.join([getVal[0][0].lower()+getVal[0][1:]])+";\n")
         else:
-            writeData.write("\tprivate float " + getVal[4] + ";\n")
+            writeData.write("\tprivate Float " + getVal[4] + ";\n")
     else:
         if aliasName == 0:
             writeData.write("\tprivate String "+''.join([getVal[0][0].lower()+getVal[0][1:]])+";\n")
